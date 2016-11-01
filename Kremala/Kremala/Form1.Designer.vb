7f541292ded251c6d83f0231d17dbf2290ebab3d
Dionysis Kalepanagos <daino92@gmail.com>
2016-11-01 10:55:10 +0200

    Add files via upload

---
+++ Kremala/Kremala/Form1.Designer.vb
@@ -0,0 +1,429 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.bt25 = New System.Windows.Forms.Button
+        Me.bt24 = New System.Windows.Forms.Button
+        Me.bt23 = New System.Windows.Forms.Button
+        Me.bt22 = New System.Windows.Forms.Button
+        Me.bt21 = New System.Windows.Forms.Button
+        Me.bt20 = New System.Windows.Forms.Button
+        Me.bt19 = New System.Windows.Forms.Button
+        Me.bt18 = New System.Windows.Forms.Button
+        Me.bt17 = New System.Windows.Forms.Button
+        Me.bt16 = New System.Windows.Forms.Button
+        Me.bt15 = New System.Windows.Forms.Button
+        Me.bt14 = New System.Windows.Forms.Button
+        Me.bt13 = New System.Windows.Forms.Button
+        Me.bt12 = New System.Windows.Forms.Button
+        Me.bt11 = New System.Windows.Forms.Button
+        Me.bt10 = New System.Windows.Forms.Button
+        Me.bt9 = New System.Windows.Forms.Button
+        Me.bt8 = New System.Windows.Forms.Button
+        Me.bt7 = New System.Windows.Forms.Button
+        Me.bt6 = New System.Windows.Forms.Button
+        Me.bt5 = New System.Windows.Forms.Button
+        Me.bt4 = New System.Windows.Forms.Button
+        Me.bt3 = New System.Windows.Forms.Button
+        Me.bt2 = New System.Windows.Forms.Button
+        Me.bt1 = New System.Windows.Forms.Button
+        Me.btEnd = New System.Windows.Forms.Button
+        Me.btLoadWord = New System.Windows.Forms.Button
+        Me.LblWord = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(402, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 87
+        Me.Label1.Text = "Ζωές"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(447, 120)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(28, 20)
+        Me.TextBox1.TabIndex = 86
+        Me.TextBox1.Text = "7"
+        '
+        'bt25
+        '
+        Me.bt25.Enabled = False
+        Me.bt25.Location = New System.Drawing.Point(307, 262)
+        Me.bt25.Name = "bt25"
+        Me.bt25.Size = New System.Drawing.Size(38, 31)
+        Me.bt25.TabIndex = 85
+        Me.bt25.Text = "z"
+        Me.bt25.UseVisualStyleBackColor = True
+        '
+        'bt24
+        '
+        Me.bt24.Enabled = False
+        Me.bt24.Location = New System.Drawing.Point(253, 262)
+        Me.bt24.Name = "bt24"
+        Me.bt24.Size = New System.Drawing.Size(38, 31)
+        Me.bt24.TabIndex = 84
+        Me.bt24.Text = "y"
+        Me.bt24.UseVisualStyleBackColor = True
+        '
+        'bt23
+        '
+        Me.bt23.Enabled = False
+        Me.bt23.Location = New System.Drawing.Point(199, 262)
+        Me.bt23.Name = "bt23"
+        Me.bt23.Size = New System.Drawing.Size(38, 31)
+        Me.bt23.TabIndex = 83
+        Me.bt23.Text = "x"
+        Me.bt23.UseVisualStyleBackColor = True
+        '
+        'bt22
+        '
+        Me.bt22.Enabled = False
+        Me.bt22.Location = New System.Drawing.Point(145, 262)
+        Me.bt22.Name = "bt22"
+        Me.bt22.Size = New System.Drawing.Size(38, 31)
+        Me.bt22.TabIndex = 82
+        Me.bt22.Text = "w"
+        Me.bt22.UseVisualStyleBackColor = True
+        '
+        'bt21
+        '
+        Me.bt21.Enabled = False
+        Me.bt21.Location = New System.Drawing.Point(91, 262)
+        Me.bt21.Name = "bt21"
+        Me.bt21.Size = New System.Drawing.Size(38, 31)
+        Me.bt21.TabIndex = 81
+        Me.bt21.Text = "u"
+        Me.bt21.UseVisualStyleBackColor = True
+        '
+        'bt20
+        '
+        Me.bt20.Enabled = False
+        Me.bt20.Location = New System.Drawing.Point(307, 225)
+        Me.bt20.Name = "bt20"
+        Me.bt20.Size = New System.Drawing.Size(38, 31)
+        Me.bt20.TabIndex = 80
+        Me.bt20.Text = "v"
+        Me.bt20.UseVisualStyleBackColor = True
+        '
+        'bt19
+        '
+        Me.bt19.Enabled = False
+        Me.bt19.Location = New System.Drawing.Point(253, 225)
+        Me.bt19.Name = "bt19"
+        Me.bt19.Size = New System.Drawing.Size(38, 31)
+        Me.bt19.TabIndex = 79
+        Me.bt19.Text = "t"
+        Me.bt19.UseVisualStyleBackColor = True
+        '
+        'bt18
+        '
+        Me.bt18.Enabled = False
+        Me.bt18.Location = New System.Drawing.Point(199, 225)
+        Me.bt18.Name = "bt18"
+        Me.bt18.Size = New System.Drawing.Size(38, 31)
+        Me.bt18.TabIndex = 78
+        Me.bt18.Text = "s"
+        Me.bt18.UseVisualStyleBackColor = True
+        '
+        'bt17
+        '
+        Me.bt17.Enabled = False
+        Me.bt17.Location = New System.Drawing.Point(145, 225)
+        Me.bt17.Name = "bt17"
+        Me.bt17.Size = New System.Drawing.Size(38, 31)
+        Me.bt17.TabIndex = 77
+        Me.bt17.Text = "r"
+        Me.bt17.UseVisualStyleBackColor = True
+        '
+        'bt16
+        '
+        Me.bt16.Enabled = False
+        Me.bt16.Location = New System.Drawing.Point(91, 225)
+        Me.bt16.Name = "bt16"
+        Me.bt16.Size = New System.Drawing.Size(38, 31)
+        Me.bt16.TabIndex = 76
+        Me.bt16.Text = "q"
+        Me.bt16.UseVisualStyleBackColor = True
+        '
+        'bt15
+        '
+        Me.bt15.Enabled = False
+        Me.bt15.Location = New System.Drawing.Point(307, 188)
+        Me.bt15.Name = "bt15"
+        Me.bt15.Size = New System.Drawing.Size(38, 31)
+        Me.bt15.TabIndex = 75
+        Me.bt15.Text = "p"
+        Me.bt15.UseVisualStyleBackColor = True
+        '
+        'bt14
+        '
+        Me.bt14.Enabled = False
+        Me.bt14.Location = New System.Drawing.Point(253, 188)
+        Me.bt14.Name = "bt14"
+        Me.bt14.Size = New System.Drawing.Size(38, 31)
+        Me.bt14.TabIndex = 74
+        Me.bt14.Text = "o"
+        Me.bt14.UseVisualStyleBackColor = True
+        '
+        'bt13
+        '
+        Me.bt13.Enabled = False
+        Me.bt13.Location = New System.Drawing.Point(199, 188)
+        Me.bt13.Name = "bt13"
+        Me.bt13.Size = New System.Drawing.Size(38, 31)
+        Me.bt13.TabIndex = 73
+        Me.bt13.Text = "n"
+        Me.bt13.UseVisualStyleBackColor = True
+        '
+        'bt12
+        '
+        Me.bt12.Enabled = False
+        Me.bt12.Location = New System.Drawing.Point(145, 188)
+        Me.bt12.Name = "bt12"
+        Me.bt12.Size = New System.Drawing.Size(38, 31)
+        Me.bt12.TabIndex = 72
+        Me.bt12.Text = "m"
+        Me.bt12.UseVisualStyleBackColor = True
+        '
+        'bt11
+        '
+        Me.bt11.Enabled = False
+        Me.bt11.Location = New System.Drawing.Point(91, 188)
+        Me.bt11.Name = "bt11"
+        Me.bt11.Size = New System.Drawing.Size(38, 31)
+        Me.bt11.TabIndex = 71
+        Me.bt11.Text = "l"
+        Me.bt11.UseVisualStyleBackColor = True
+        '
+        'bt10
+        '
+        Me.bt10.Enabled = False
+        Me.bt10.Location = New System.Drawing.Point(307, 151)
+        Me.bt10.Name = "bt10"
+        Me.bt10.Size = New System.Drawing.Size(38, 31)
+        Me.bt10.TabIndex = 70
+        Me.bt10.Text = "k"
+        Me.bt10.UseVisualStyleBackColor = True
+        '
+        'bt9
+        '
+        Me.bt9.Enabled = False
+        Me.bt9.Location = New System.Drawing.Point(253, 151)
+        Me.bt9.Name = "bt9"
+        Me.bt9.Size = New System.Drawing.Size(38, 31)
+        Me.bt9.TabIndex = 69
+        Me.bt9.Text = "i"
+        Me.bt9.UseVisualStyleBackColor = True
+        '
+        'bt8
+        '
+        Me.bt8.Enabled = False
+        Me.bt8.Location = New System.Drawing.Point(199, 151)
+        Me.bt8.Name = "bt8"
+        Me.bt8.Size = New System.Drawing.Size(38, 31)
+        Me.bt8.TabIndex = 68
+        Me.bt8.Text = "h"
+        Me.bt8.UseVisualStyleBackColor = True
+        '
+        'bt7
+        '
+        Me.bt7.Enabled = False
+        Me.bt7.Location = New System.Drawing.Point(144, 151)
+        Me.bt7.Name = "bt7"
+        Me.bt7.Size = New System.Drawing.Size(38, 31)
+        Me.bt7.TabIndex = 67
+        Me.bt7.Text = "g"
+        Me.bt7.UseVisualStyleBackColor = True
+        '
+        'bt6
+        '
+        Me.bt6.Enabled = False
+        Me.bt6.Location = New System.Drawing.Point(91, 151)
+        Me.bt6.Name = "bt6"
+        Me.bt6.Size = New System.Drawing.Size(38, 31)
+        Me.bt6.TabIndex = 66
+        Me.bt6.Text = "f"
+        Me.bt6.UseVisualStyleBackColor = True
+        '
+        'bt5
+        '
+        Me.bt5.Enabled = False
+        Me.bt5.Location = New System.Drawing.Point(307, 114)
+        Me.bt5.Name = "bt5"
+        Me.bt5.Size = New System.Drawing.Size(38, 31)
+        Me.bt5.TabIndex = 65
+        Me.bt5.Text = "e"
+        Me.bt5.UseVisualStyleBackColor = True
+        '
+        'bt4
+        '
+        Me.bt4.Enabled = False
+        Me.bt4.Location = New System.Drawing.Point(253, 114)
+        Me.bt4.Name = "bt4"
+        Me.bt4.Size = New System.Drawing.Size(38, 31)
+        Me.bt4.TabIndex = 64
+        Me.bt4.Text = "d"
+        Me.bt4.UseVisualStyleBackColor = True
+        '
+        'bt3
+        '
+        Me.bt3.Enabled = False
+        Me.bt3.Location = New System.Drawing.Point(199, 114)
+        Me.bt3.Name = "bt3"
+        Me.bt3.Size = New System.Drawing.Size(38, 31)
+        Me.bt3.TabIndex = 63
+        Me.bt3.Text = "c"
+        Me.bt3.UseVisualStyleBackColor = True
+        '
+        'bt2
+        '
+        Me.bt2.Enabled = False
+        Me.bt2.Location = New System.Drawing.Point(145, 114)
+        Me.bt2.Name = "bt2"
+        Me.bt2.Size = New System.Drawing.Size(38, 31)
+        Me.bt2.TabIndex = 62
+        Me.bt2.Text = "b"
+        Me.bt2.UseVisualStyleBackColor = True
+        '
+        'bt1
+        '
+        Me.bt1.Enabled = False
+        Me.bt1.Location = New System.Drawing.Point(91, 114)
+        Me.bt1.Name = "bt1"
+        Me.bt1.Size = New System.Drawing.Size(38, 31)
+        Me.bt1.TabIndex = 61
+        Me.bt1.Text = "a"
+        Me.bt1.UseVisualStyleBackColor = True
+        '
+        'btEnd
+        '
+        Me.btEnd.Location = New System.Drawing.Point(234, 313)
+        Me.btEnd.Margin = New System.Windows.Forms.Padding(2)
+        Me.btEnd.Name = "btEnd"
+        Me.btEnd.Size = New System.Drawing.Size(131, 58)
+        Me.btEnd.TabIndex = 60
+        Me.btEnd.Text = "Τέλος"
+        Me.btEnd.UseVisualStyleBackColor = True
+        '
+        'btLoadWord
+        '
+        Me.btLoadWord.Location = New System.Drawing.Point(53, 315)
+        Me.btLoadWord.Margin = New System.Windows.Forms.Padding(2)
+        Me.btLoadWord.Name = "btLoadWord"
+        Me.btLoadWord.Size = New System.Drawing.Size(129, 55)
+        Me.btLoadWord.TabIndex = 59
+        Me.btLoadWord.Text = "Φόρτωσε μια λέξη"
+        Me.btLoadWord.UseVisualStyleBackColor = True
+        '
+        'LblWord
+        '
+        Me.LblWord.BackColor = System.Drawing.SystemColors.Control
+        Me.LblWord.Font = New System.Drawing.Font("Courier New", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.LblWord.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.LblWord.Location = New System.Drawing.Point(22, 9)
+        Me.LblWord.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LblWord.Name = "LblWord"
+        Me.LblWord.Size = New System.Drawing.Size(397, 93)
+        Me.LblWord.TabIndex = 58
+        Me.LblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 387)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.bt25)
+        Me.Controls.Add(Me.bt24)
+        Me.Controls.Add(Me.bt23)
+        Me.Controls.Add(Me.bt22)
+        Me.Controls.Add(Me.bt21)
+        Me.Controls.Add(Me.bt20)
+        Me.Controls.Add(Me.bt19)
+        Me.Controls.Add(Me.bt18)
+        Me.Controls.Add(Me.bt17)
+        Me.Controls.Add(Me.bt16)
+        Me.Controls.Add(Me.bt15)
+        Me.Controls.Add(Me.bt14)
+        Me.Controls.Add(Me.bt13)
+        Me.Controls.Add(Me.bt12)
+        Me.Controls.Add(Me.bt11)
+        Me.Controls.Add(Me.bt10)
+        Me.Controls.Add(Me.bt9)
+        Me.Controls.Add(Me.bt8)
+        Me.Controls.Add(Me.bt7)
+        Me.Controls.Add(Me.bt6)
+        Me.Controls.Add(Me.bt5)
+        Me.Controls.Add(Me.bt4)
+        Me.Controls.Add(Me.bt3)
+        Me.Controls.Add(Me.bt2)
+        Me.Controls.Add(Me.bt1)
+        Me.Controls.Add(Me.btEnd)
+        Me.Controls.Add(Me.btLoadWord)
+        Me.Controls.Add(Me.LblWord)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents bt25 As System.Windows.Forms.Button
+    Friend WithEvents bt24 As System.Windows.Forms.Button
+    Friend WithEvents bt23 As System.Windows.Forms.Button
+    Friend WithEvents bt22 As System.Windows.Forms.Button
+    Friend WithEvents bt21 As System.Windows.Forms.Button
+    Friend WithEvents bt20 As System.Windows.Forms.Button
+    Friend WithEvents bt19 As System.Windows.Forms.Button
+    Friend WithEvents bt18 As System.Windows.Forms.Button
+    Friend WithEvents bt17 As System.Windows.Forms.Button
+    Friend WithEvents bt16 As System.Windows.Forms.Button
+    Friend WithEvents bt15 As System.Windows.Forms.Button
+    Friend WithEvents bt14 As System.Windows.Forms.Button
+    Friend WithEvents bt13 As System.Windows.Forms.Button
+    Friend WithEvents bt12 As System.Windows.Forms.Button
+    Friend WithEvents bt11 As System.Windows.Forms.Button
+    Friend WithEvents bt10 As System.Windows.Forms.Button
+    Friend WithEvents bt9 As System.Windows.Forms.Button
+    Friend WithEvents bt8 As System.Windows.Forms.Button
+    Friend WithEvents bt7 As System.Windows.Forms.Button
+    Friend WithEvents bt6 As System.Windows.Forms.Button
+    Friend WithEvents bt5 As System.Windows.Forms.Button
+    Friend WithEvents bt4 As System.Windows.Forms.Button
+    Friend WithEvents bt3 As System.Windows.Forms.Button
+    Friend WithEvents bt2 As System.Windows.Forms.Button
+    Friend WithEvents bt1 As System.Windows.Forms.Button
+    Friend WithEvents btEnd As System.Windows.Forms.Button
+    Friend WithEvents btLoadWord As System.Windows.Forms.Button
+    Friend WithEvents LblWord As System.Windows.Forms.Label
+
+End Class
---
+++ Kremala/Kremala/Form1.vb
@@ -0,0 +1,126 @@
+﻿Public Class Form1
+    Dim word As String
+    Dim display As String
+
+    Private Sub readWordFromFile()
+        'Σαυτή τη διαδικασία ανοίγουμε το αρχείο c:\words.txt και διαβάζουμε όλες τις γραμμές του
+        'Ταυτόχρονα μετράμε πόσες φορές έγινε αυτό και το αποθηκεύουμε στη μεταβλητή NumberOfWords
+        'Στη συνέχεια κλείνουμε το αρχείο & παράγουμε έναν τυχαίο αριθμό num στο διάστημα [1, NumberOfWords]
+        'Ξανα-ανοίγουμε το αρχείο c:\words.txt & διαβάζουμε τη λέξη που αντιστοιχεί στον τυχαίο αριθμό num
+        'Τη λέξη αυτή την εμφανίζουμε στο πλαίσιο LblWord & κλείνουμε το αρχείο
+
+        Dim NumberOfWords As Integer 'Μετρά το πλήθος των γραμμών του αρχείου
+        Dim num As Integer 'Κρατά τον τυχαίο αριθμό
+
+        'Ανοιγμα του αρχείου για ανάγνωση με χειριστή αρχείου τον αριθμό 1
+        FileOpen(1, "c:\words.txt", OpenMode.Input)
+
+        'Οσο στο αρχείο με χειριστή 1 ΔΕΝ έχει γίνει ανάγνωση του σημείου End Of File
+        'δηλαδή όσο στο αρχείο υπάρχουν δεδομένα, διαβάζουμε γραμμές
+        Do While Not EOF(1)
+            LineInput(1) 'γίνεται ανάγνωση γραμμής από το 1 χωρίς να τοποθετούμε τίποτα σε μεταβλητή
+            NumberOfWords = NumberOfWords + 1 'αυξάνουμε τον μετρητή κατά 1 μονάδα
+        Loop
+        FileClose(1) 'κλείνουμε το αρχείο με χειριστή 1
+
+        'παράγουμε έναν τυχαίο αριθμό στο διάστημα [1, NumberOfWords]
+        'όπου NumberOfWords είναι το πλήθος των γραμμών-λέξεων στο αρχείο
+        num = Int(Rnd() * NumberOfWords + 1)
+
+        FileOpen(1, "c:\words.txt", OpenMode.Input) 'ανοίγουμε πάλι το αρχείο με χειριστή πάλι 1
+        For I = 1 To num - 1
+            LineInput(1) 'διαβάζουμε πάλι όλες τις γραμμές μέχρι τη γραμμή (num-1)
+        Next
+        Input(1, word) 'διαβάζουμε τη λέξη που βρίσκεται στη num γραμμή
+        display = MakeDisplay(word)
+        LblWord.Text = display 'τοποθετούμε τη λέξη στο πλαίσιο LblWord
+        FileClose(1) 'κλείνουμε το αρχείο με χειριστή 1
+    End Sub
+
+    Private Sub Bt1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt1.Click, bt2.Click, bt3.Click, bt4.Click, bt5.Click, bt6.Click, bt7.Click, bt8.Click, bt9.Click, bt10.Click, bt11.Click, bt12.Click, bt13.Click, bt14.Click, bt15.Click, bt16.Click, bt17.Click, bt18.Click, bt19.Click, bt20.Click, bt21.Click, bt22.Click, bt23.Click, bt24.Click
+        Dim c As Byte, found As Boolean
+        found = False 'σημαία που γίνεται true όταν το γράμμα letter βρεθεί στη λέξη w
+        Dim ctl As Control
+        For c = 2 To Len(word) - 1
+            If Mid(word, c, 1) = sender.text Then
+                display = LSet(display, c - 1) & sender.text & Microsoft.VisualBasic.Right(display, Len(display) - c)
+                LblWord.Text = display
+                'το νέο display φτιάχνεται ενώνοντας τα κομμάτια: left (μέχρι τη θέση του γράμματος letter)
+                'το γράμμα letter που βρέθηκε ΚΑΙ 
+                'το right(παίρνοντας χαρακτήρες από το τέλος της λέξης display μέχρι το γράμμα letter
+                found = True
+            End If
+            sender.enabled = False
+        Next
+        If display = word Then
+            MsgBox("Μπράβο! Βρήκες τη λέξη!")
+            For Each ctl In Me.Controls
+                If TypeOf ctl Is Button Then
+                    ctl.Enabled = False
+                    btEnd.Enabled = True
+                End If
+                btLoadWord.Enabled = True
+            Next
+        End If
+        If found = False Then
+            TextBox1.Text -= 1
+        End If
+        If TextBox1.Text = 0 Then
+            MsgBox("Δυστυχώς έχασες :( , η λέξη ήταν: " & word)
+            TextBox1.Text = 7
+            For Each ctl In Me.Controls
+                If TypeOf ctl Is Button Then
+                    ctl.Enabled = False
+                    btEnd.Enabled = True
+                    btLoadWord.Enabled = True
+                End If
+            Next
+        End If
+    End Sub
+
+    Public Function MakeDisplay(ByVal w As String) As String 'Φτιάχνει τη λέξη της κρεμάλας με παυλίτσες στα ενδιάμεσα
+        Dim display As String, c As Byte
+        display = LSet(w, 1)
+        For c = 2 To Len(w) - 1
+            display = display & "-"
+        Next
+        display = display & Microsoft.VisualBasic.Right(w, 1)
+        Return display
+    End Function
+
+    Private Sub btLoadWord_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btLoadWord.Click
+        TextBox1.Text = 7
+        Call readWordFromFile()
+        For Each ctl In Me.Controls
+            If TypeOf ctl Is Button Then
+                ctl.Enabled = True
+            End If
+        Next
+    End Sub
+
+    Private Sub btEnd_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btEnd.Click
+        Call writeNameToFile()
+        TextBox1.Text = 7
+        End
+    End Sub
+
+    Private Sub writeNameToFile()
+        'Ζητά ένα όνομα και το προσθέτει στα περιεχόμενα του αρχείου c:\players.txt
+        'μαζί με την ημερομηνία και ώρα του συστήματος εκείνη τη χρονική στιγμή
+
+        Dim yourName As String 'Το όνομα του παίκτη που θα αποθηκευτεί στο αρχείο
+
+        'Ανοιγμα του αρχείου για εγγραφή με προσθήκη (Append)
+        'Αν το ανοίξουμε με OpenMode.Output χάνουμε τα περιεχόμενα που είχε μέχρι τώρα
+        FileOpen(5, "c:\players.txt", OpenMode.Append)
+
+        yourName = InputBox("Δώσε το όνομά σου", "Name", "Όνομα..")
+        WriteLine(5, yourName, Now)
+
+        FileClose(5) 'κλείνουμε το αρχείο με χειριστή 2
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Call readWordFromFile()
+    End Sub
+End Class
